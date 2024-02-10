e2f79deb147e4fb81b406fabd1daf77962ea5a92
Haro Mikhaylov <www.haris@bk.ru>
2024-02-10 20:35:08 +0300

    Initial commit

---
+++ blogicum/pages/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+app_name = 'pages'
+
+
+def about(request):
+    template = 'pages/about.html'
+    return render(request, template)
+
+
+def rules(request):
+    template = 'pages/rules.html'
+    return render(request, template)
