ea6970426a679d001ad8fb95a46b8d96373de5dc
Haro Mikhaylov <www.haris@bk.ru>
2024-02-16 19:37:53 +0300

    Stable NEW

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,35 +1,54 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Post
 from django.http import Http404
-from django.shortcuts import render
-from blog.models import Post
 from django.utils import timezone
 
 app_name = 'blog'
 
 
 def index(request):
-    template = 'blog/index.html'
-    posts = Post.objects.all().filter(
+    posts = Post.objects.all(
+    ).filter(
         category__is_published=True,
         is_published=True,
         pub_date__lte=timezone.now()
-    ).order_by('-pub_date')[:5]
+    ).order_by(
+        '-pub_date'
+    )[:5]
+    template = 'blog/index.html'
     context = {'post_list': posts}
     return render(request, template, context)
 
 
 def detail(request, post_id):
+    post = Post.objects.all(
+    ).filter(
+        category__is_published=True,
+        is_published=True,
+        pub_date__lte=timezone.now()
+    ).order_by(
+        '-pub_date'
+    )[post_id]
     template = 'blog/detail.html'
-    posts_by_id = {post['id']: post for post in posts}
     try:
-        post = posts_by_id[post_id]
-    except KeyError:
+        post
+    except Exception:
         raise Http404('Пост не найден')
     context = {'post': post}
     return render(request, template, context)
 
 
 def category(request, category_slug):
+    category_obj = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True
+    )
+    posts = category_obj.post_set.filter(
+        is_published=True,
+        pub_date__lte=timezone.now()).order_by('-pub_date')
+
     template = 'blog/category.html'
     context = {'post_list': posts,
-               'category_slug': category_slug}
+               'category': category_obj}
     return render(request, template, context)
